fixed tv calculation

patch/nn_modules.py
@@ -62,7 +62,7 @@ class FaceXZooProjector(nn.Module):
         self.max_contrast = 1.2
         self.min_brightness = -0.1
         self.max_brightness = 0.1
-        self.noise_factor = 0.10
+        self.noise_factor = 0.05
 
     def forward(self, img_batch, landmarks, adv_patch, uv_mask_src=None, do_aug=False):
         pos, vertices = self.get_vertices(landmarks, img_batch)
@@ -490,24 +490,11 @@ def read_landmark_106_array(face_lms):
 class TotalVariation(nn.Module):
     def __init__(self, device) -> None:
         super(TotalVariation, self).__init__()
+        self.device = device
         self.uv_mask_src = transforms.ToTensor()(Image.open('../prnet/new_uv.png').convert('L')).to(device).squeeze()
-        non_zero_indices = torch.nonzero(self.uv_mask_src)
-        self.left = torch.min(non_zero_indices[..., 1])
-        self.right = torch.max(non_zero_indices[..., 1])
-        self.bottom = torch.min(non_zero_indices[..., 0])
-        self.top = torch.max(non_zero_indices[..., 0])
+        self.number_of_pixels = torch.count_nonzero(self.uv_mask_src)
 
-    def forward(self, adv_patch, train=True):
-        tv_patch = adv_patch[..., self.bottom:self.top, self.left:self.right]
-        if train:
-            tv_patch.register_hook(self.zero_grads)
-        # transforms.ToPILImage()(tv_patch.detach().cpu().squeeze()).show()
-        loss = total_variation(tv_patch) / torch.numel(tv_patch)
-        # h_tv = F.l1_loss(adv_patch[..., 1:, :], adv_patch[..., :-1, :], reduction='mean')  # calc height tv
-        # w_tv = F.l1_loss(adv_patch[..., :, 1:], adv_patch[..., :, :-1], reduction='mean')  # calc width tv
-        # loss = h_tv + w_tv
+    def forward(self, adv_patch):
+        loss = total_variation(adv_patch * self.uv_mask_src) / self.number_of_pixels
         return loss
 
-    def zero_grads(self, grads):
-        tv_mask = self.uv_mask_src[self.bottom:self.top, self.left:self.right]
-        grads[:, :, tv_mask == 0] = 0

patch/train.py
@@ -199,7 +199,7 @@ class AdversarialMask:
         # transforms.ToPILImage()(patch.squeeze(0) * uv_face).show()
         return patch
 
-    def forward_step(self, img_batch, adv_patch_cpu, img_names, cls_id, train=True):
+    def forward_step(self, img_batch, adv_patch_cpu, img_names, cls_id):
         img_batch = img_batch.to(device)
         adv_patch = adv_patch_cpu.to(device)
         cls_id = cls_id.to(device)
@@ -212,7 +212,7 @@ class AdversarialMask:
         for embedder_name, emb_model in self.embedders.items():
             patch_embs[embedder_name] = emb_model(img_batch_applied)
 
-        tv_loss = self.total_variation(adv_patch, train)
+        tv_loss = self.total_variation(adv_patch)
         loss = self.loss_fn(patch_embs, tv_loss, cls_id)
 
         return loss, [img_batch, adv_patch, img_batch_applied, patch_embs, tv_loss]
@@ -276,7 +276,6 @@ class AdversarialMask:
         final_patch_img.save(self.config.current_dir + '/final_results/final_patch.png', 'PNG')
         new_size = tuple(self.config.magnification_ratio * s for s in self.config.img_size)
         transforms.Resize(new_size)(final_patch_img).save(self.config.current_dir + '/final_results/final_patch_magnified.png', 'PNG')
-
         torch.save(self.best_patch, self.config.current_dir + '/final_results/final_patch_raw.pt')
 
         with open(self.config.current_dir + '/losses/train_losses', 'wb') as fp: